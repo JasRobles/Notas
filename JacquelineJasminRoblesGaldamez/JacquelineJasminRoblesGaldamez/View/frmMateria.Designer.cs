15e0a79de5d61c022998db5852fadb999e6c1e32
JasRobles <63456629+JasRobles@users.noreply.github.com>
2020-04-17 00:06:35 -0600

    proceso

--- JacquelineJasminRoblesGaldamez/JacquelineJasminRoblesGaldamez/View/frmMateria.Designer.cs
+++ JacquelineJasminRoblesGaldamez/JacquelineJasminRoblesGaldamez/View/frmMateria.Designer.cs
@@ -32,6 +32,9 @@
             this.btnEditar = new System.Windows.Forms.Button();
             this.btnGuardar = new System.Windows.Forms.Button();
             this.dgvMaterias = new System.Windows.Forms.DataGridView();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvMaterias)).BeginInit();
             this.SuspendLayout();
             // 
@@ -43,6 +46,7 @@
             this.btnEliminar.TabIndex = 15;
             this.btnEliminar.Text = "Eliminar";
             this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
             // 
             // btnEditar
             // 
@@ -52,6 +56,7 @@
             this.btnEditar.TabIndex = 14;
             this.btnEditar.Text = "Editar";
             this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
             // 
             // btnGuardar
             // 
@@ -61,6 +66,7 @@
             this.btnGuardar.TabIndex = 13;
             this.btnGuardar.Text = "Guardar";
             this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
             // 
             // dgvMaterias
             // 
@@ -68,17 +74,45 @@
             this.dgvMaterias.AllowUserToDeleteRows = false;
             this.dgvMaterias.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvMaterias.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvMaterias.Location = new System.Drawing.Point(12, 66);
+            this.dgvMaterias.Location = new System.Drawing.Point(26, 75);
             this.dgvMaterias.Name = "dgvMaterias";
             this.dgvMaterias.ReadOnly = true;
             this.dgvMaterias.Size = new System.Drawing.Size(665, 329);
             this.dgvMaterias.TabIndex = 12;
+            this.dgvMaterias.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvMaterias_CellContentClick);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(49, 49);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(123, 20);
+            this.txtNombre.TabIndex = 16;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 13);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Nombre de materia";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(239, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 21;
             // 
             // frmMateria
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(898, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNombre);
             this.Controls.Add(this.btnEliminar);
             this.Controls.Add(this.btnEditar);
             this.Controls.Add(this.btnGuardar);
@@ -87,8 +121,10 @@
             this.Name = "frmMateria";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmMateria";
+            this.Load += new System.EventHandler(this.frmMateria_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvMaterias)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -98,5 +134,8 @@
         private System.Windows.Forms.Button btnEditar;
         private System.Windows.Forms.Button btnGuardar;
         private System.Windows.Forms.DataGridView dgvMaterias;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- JacquelineJasminRoblesGaldamez/JacquelineJasminRoblesGaldamez/View/frmMateria.cs
+++ JacquelineJasminRoblesGaldamez/JacquelineJasminRoblesGaldamez/View/frmMateria.cs
@@ -7,14 +7,149 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using JacquelineJasminRoblesGaldamez.Model;
 
 namespace JacquelineJasminRoblesGaldamez.View
 {
     public partial class frmMateria : Form
     {
+        Materias Mat = new Materias();
+        DialogResult resultado;
+
         public frmMateria()
         {
             InitializeComponent();
+
+            Cargardatos();
+
+            Eliminar();
+            offEditar();
+        }
+
+        public void Cargardatos()
+        {
+            using (ControlNotasEntities db = new ControlNotasEntities())
+            {
+                var list = from Mater in db.Materias
+                           select new { Id = Mater.idMateria, Nombre = Mater.Nombre };
+                dgvMaterias.DataSource = list.ToList();
+
+
+            }
+        }
+        public void Eliminar()
+        {
+            txtNombre.Text = "";
+        }
+
+        public void offEditar()
+        {
+            btnEditar.Enabled = false;
+            btnEliminar.Enabled = false;
+            btnGuardar.Enabled = true;
+        }
+        public void onEditar()
+        {
+            btnEditar.Enabled = true;
+            btnEliminar.Enabled = true;
+            btnGuardar.Enabled = false;
+
+        }
+
+        private void frmMateria_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            using (ControlNotasEntities db = new ControlNotasEntities())
+            {
+                Mat.Nombre = txtNombre.Text;
+                db.Materias.Add(Mat);
+                db.SaveChanges();
+                Cargardatos();
+                Eliminar();
+
+
+            }
+            
+            
+
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            resultado = MessageBox.Show("¿Esta seguro de realizar el cambio?", "Notificacion", MessageBoxButtons.YesNo,
+                MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1);
+            if (resultado == DialogResult.Yes)
+            {
+                using (ControlNotasEntities db = new ControlNotasEntities())
+                {
+                    string id = dgvMaterias.CurrentRow.Cells[0].Value.ToString();
+                    int Id = int.Parse(id);
+                    Mat = db.Materias.Where(verificarid => verificarid.idMateria == Id).First();
+                    Mat.Nombre = txtNombre.Text;
+                    db.Entry(Mat).State = System.Data.Entity.EntityState.Modified;
+                    db.SaveChanges();
+                    Cargardatos();
+                    Eliminar();
+                    offEditar();
+
+                }
+
+
+            }
+            else
+            {
+                Cargardatos();
+                Eliminar();
+                offEditar();
+            }
+
+
+        }
+
+        private void dgvMaterias_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            string Nombre = dgvMaterias.CurrentRow.Cells[1].Value.ToString();
+            txtNombre.Text = Nombre;
+            onEditar();
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            resultado = MessageBox.Show("¿Esta seguro que desea eliminar los datos?", "Notificacion", MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question, MessageBoxDefaultButton.Button1);
+
+            if (resultado == DialogResult.Yes)
+            {
+
+                using (ControlNotasEntities db = new ControlNotasEntities())
+                {
+
+                    string id = dgvMaterias.CurrentRow.Cells[0].Value.ToString();
+                    int Id = int.Parse(id);
+                    Mat = db.Materias.Find(Id);
+                    Mat.Nombre = txtNombre.Text;
+                    db.Materias.Remove(Mat);
+                    db.SaveChanges();
+                    Cargardatos();
+                    Eliminar();
+                    offEditar();
+
+
+                }
+
+            }
+            else
+            {
+                Cargardatos();
+                Eliminar();
+                offEditar();
+                
+            }
         }
     }
 }
